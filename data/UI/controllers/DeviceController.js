77b0e14ed50dc50ed01ff2cfce12e39d7ec5513c
OffByNone <jeremy.mcneil@gmail.com>
2015-03-25 19:15:55 -0500

    wayyyy too many changes
    Added keyboard shortcuts: left arrow, right arrow, space and delete
    Added toast notifications when events happen, which turn into spam very
    quickly
    Removed some unneeded files
    Fixed event subscription issues
    Searches on interval of 7.5 seconds
    removed the requestjs lib and replaced it with fetch
    added load function to deviceservices.
    If at last file in playlist and repeat is off, when calling next it will
    load the first file but stop playback
    Cleaned up some promises on the MediaReceiverService
    Rebuilt XPI

--- data/UI/controllers/DeviceController.js
+++ data/UI/controllers/DeviceController.js
@@ -1,30 +1,25 @@
 "use strict";
 
-rotaryApp.controller('DeviceController', function DeviceController($scope, $routeParams, eventService) {
+rotaryApp.controller('DeviceController', function DeviceController($scope, $routeParams, eventService, $window) {
 	$scope.title = 'Devices on your network:';
 	$scope.devices = [];
 	$scope.deviceTypes = [];
 	$scope.serviceTypes = [];
 	$scope.deviceId = $routeParams.deviceId;
 	$scope.playlist = [];
-	$scope.activeFile = {};
-	$scope.filePicker = { };
+	$scope.currentTrack = {};
+	$scope.currentMedia = {};
+	$scope.filePicker = {};
 	$scope.filePickerOpen = true;
+	$scope.playback = {};
 
-	//{name: "test1", currentTime:"2:35", currentTimeInSeconds: 155, duration: "3:15", durationInSeconds: 195, name: "test track 1", path= "file path here" }
 
 	$scope.currentFilePercent = function currentFilePercent() {
-		return $scope.activeFile == null ? null : ($scope.activeFile.currentTimeInSeconds / $scope.activeFile.durationInSeconds) * 100;
+		return $scope.currentTrack == null ? null : ($scope.currentTrack.currentTimeInSeconds / $scope.currentTrack.durationInSeconds) * 100;
 	};
 	$scope.canExecuteAction = function (action) {
 		return $scope.availableActions == null ? true : $scope.availableActions.every(availableAction => availableAction != action);
 	};
-	function setState(newState) {
-		$scope.playback = $scope.playback || {};
-		$scope.playback.state = newState;
-	}
-
-
 	$scope.play = function play() {
 		setState('Play');
 		eventService.emit('play', $scope.activeDevice);
@@ -39,18 +34,20 @@ rotaryApp.controller('DeviceController', function DeviceController($scope, $rout
 	};
 	$scope.previous = function previous() {
 		for (var i in $scope.playlist)
-			if ($scope.playlist.hasOwnProperty(i) && $scope.playlist[i] === $scope.activeFile)
+			if ($scope.playlist.hasOwnProperty(i) && $scope.playlist[i] === $scope.currentTrack.activeFile)
 				if (i == 0) return $scope.launch($scope.playlist[$scope.playlist.length - 1]); //we are the first file, so launch the last file
 				else return $scope.launch($scope.playlist[Number(i) - 1]); //we are not the first file so launch the previous
 	}
 	$scope.next = function next(abideByRepeat) {
 		for (var i in $scope.playlist)
-			if ($scope.playlist.hasOwnProperty(i) && $scope.playlist[i] === $scope.activeFile)
-				if (i == $scope.playlist.length - 1 && (!abideByRepeat || $scope.playback.repeat)) return $scope.launch($scope.playlist[0]); //we are the last file, so launch the first file
+			if ($scope.playlist.hasOwnProperty(i) && $scope.playlist[i] === $scope.currentTrack.activeFile)
+				if (i == $scope.playlist.length - 1)
+					if (!abideByRepeat || $scope.playback.repeat) return $scope.launch($scope.playlist[0]); //we are the last file and either pushed the next button, or repeat is enabled, so launch the first file
+					else {//we are the last file, didn't push the button and repeat is not enabled.  Stop playback, then load the first file
+						$scope.stop();
+						$scope.load($scope.playlist[0]);
+					}
 				else return $scope.launch($scope.playlist[Number(i) + 1]); //we are not the last file so launch the next
-	};
-	$scope.pickLocalFile = function pickLocalFile() {
-		eventService.emit('chooseFile', $scope.activeDevice, $scope.activeDevice.fileType);
 	};
 	$scope.toggleMute = function toggleMute(device) {
 		eventService.emit('toggleMute', device);
@@ -66,13 +63,148 @@ rotaryApp.controller('DeviceController', function DeviceController($scope, $rout
 	$scope.decrementVolume = function decrementVolume(device) {
 		$scope.setVolume(device, Number(device.volume) - 1);
 	};
-	$scope.refreshDevices = function refreshDevices() {
-		eventService.emit('refreshDevices');
-	};
-	$scope.loadDevices = function loadDevices() {
-		eventService.emit("loadDevices");
-	};
+	$scope.togglePlayState = function togglePlayState(device) {
+		if ($scope.playback.isPlaying) $scope.pause();
+		else $scope.play();
 
+		$scope.playback.isPlaying = !$scope.playback.isPlaying;
+	};
+	function setState(newState) {
+		$scope.playback.state = newState;
+		$scope.playback.isPlaying = newState.toLowerCase() == "playing";
+	}
+
+
+	function setActiveFile(fileUri) {
+		if (fileUri != null) {
+			var file = getFile(fileUri);
+
+			if (file == null) {
+				file = { name: decodeURI(getNameFromUrl(fileUri)), path: fileUri };
+				$scope.playlist.push(file);
+			}
+		}
+		$scope.currentTrack.activeFile = file;
+
+		return $scope.currentTrack.activeFile;
+	}
+	function setFileInformation(event) {
+
+		if (event.hasOwnProperty('CurrentTrackURI')) {
+			$scope.currentTrack.uri = event.CurrentTrackURI;
+			//setActiveFile($scope.currentTrack.uri); todo: dont have the uri of local files yet so I can't properly do this comparison.
+		}
+
+		if (event.hasOwnProperty('AVTransportURI')) $scope.currentMedia.uri = event.AVTransportURI;
+		if (event.hasOwnProperty('CurrentTrackMetaData')) $scope.currentTrack.metadata = event.CurrentTrackMetaData;
+		if (event.hasOwnProperty('AVTransportURIMetaData')) $scope.currentMedia.metadata = event.AVTransportURIMetaData;
+		if (event.hasOwnProperty('CurrentTrackDuration')) $scope.currentTrack.duration = event.CurrentTrackDuration;
+		if (event.hasOwnProperty('CurrentMediaDuration')) $scope.currentMedia.duration = event.CurrentMediaDuration;
+		if (event.hasOwnProperty("RelTime")) $scope.currentTrack.currentTime = event.RelTime;
+		if (event.hasOwnProperty('CurrentTrack')) $scope.currentTrack.trackNumber = event.CurrentTrack;
+	}
+
+
+
+	$scope.addToPlaylist = function addToPlaylist() {
+		//Adds files to playlist from file picker box and returns the added files
+		var files = getChosenFiles();
+		files.forEach(file => $scope.playlist.push(file));
+
+		return files;
+	};
+	$scope.removeFromPlaylist = function removeFromPlaylist(file) {
+		for (var i in $scope.playlist)
+			if ($scope.playlist.hasOwnProperty(i) && $scope.playlist[i] === file) {
+				if (file === $scope.currentTrack.activeFile) {
+					if ($scope.playlist.length > 1) $scope.next(true);
+					else $scope.stop();
+				}
+				$scope.playlist.splice(i, 1);
+				break;
+			}
+	};
+	$scope.clearPlaylist = function clearPlaylist() {
+		$scope.playlist = [];
+		$scope.stop();
+	};
+	$scope.randomizePlaylist = function randomizePlaylist() {
+		shuffle($scope.playlist);
+	};
+	$scope.launch = function launch(file) {
+		//todo: eventually change this to call load -> play, to do that however we will need to get
+		//events from the backend when load has succeeded so we know when it is safe to call play
+
+		//if a file is passed in, use that. Otherwise pull from the file picker box
+		var files = [];
+
+		if (!file) // no file is passed in, try to get a file from the file picker box
+		{
+			files = $scope.addToPlaylist();
+			if (files.length == 0) return null; //no file in the picker box either, return null.  todo: probably handle this better
+		}
+		else
+			files.push(file);
+
+		//todo: this is going to cause the same files to be mapped more than once
+		//should make sure on the backend that if the file is already mapped we don't map it again
+		//also should throw in something random into the path of the map in case someone wants to
+		//play two files of the same name --maybe throw a folder with the name of a guid in the path
+		eventService.emit('launch', $scope.activeDevice, files[0]);
+		$scope.currentTrack.activeFile = files[0];
+	};
+	$scope.load = function load(file) {
+		//if a file is passed in, use that. Otherwise pull from the file picker box
+		var files = [];
+
+		if (!file) // no file is passed in, try to get a file from the file picker box
+		{
+			files = $scope.addToPlaylist();
+			if (files.length == 0) return null; //no file in the picker box either, return null.  todo: probably handle this better
+		}
+		else
+			files.push(file);
+
+		//todo: this is going to cause the same files to be mapped more than once
+		//should make sure on the backend that if the file is already mapped we don't map it again
+		//also should throw in something random into the path of the map in case someone wants to
+		//play two files of the same name --maybe throw a folder with the name of a guid in the path
+		eventService.emit('load', $scope.activeDevice, files[0]);
+		$scope.currentTrack.activeFile = files[0];
+	};
+	$scope.pickLocalFile = function pickLocalFile() {
+		eventService.emit('chooseFile', $scope.activeDevice, $scope.activeDevice.fileType);
+	};
+	function getFile(fileUri) {
+		var file = $scope.playlist == null ? null : $scope.playlist.filter(item => item.path === fileUri);
+		return file == null ? null : file[0];
+	}
+	function getChosenFiles() {
+		//todo: pull in the file info - such as duration, artist, song/video name
+		var files = [];
+
+		if ($scope.filePicker.urlPath && $scope.filePicker.urlPath.length > 0) {
+			try {
+				new URL($scope.filePicker.urlPath);
+			}
+			catch (e) {
+				//todo: invalid url should report that to user and abort
+			}
+			$scope.filePicker.url = {
+				path: $scope.filePicker.urlPath,
+				name: $scope.filePicker.urlPath.replace(/^.*(\\|\/|\:)/, '')//set file name from url
+			};
+
+			files.push($scope.filePicker.url);
+		}
+		else if ($scope.filePicker.localFiles && $scope.filePicker.localFiles.length > 0)
+			files = $scope.filePicker.localFiles;
+		else return []; //no files are selected and url isn't set so don't do anything, just return
+
+		$scope.filePicker = {}; //clear out the url and local file fields
+
+		return files;
+	}
 	function setNewFiles(files) {
 		files.forEach(file => file.isLocal = true);
 
@@ -91,84 +223,41 @@ rotaryApp.controller('DeviceController', function DeviceController($scope, $rout
 			$scope.filePicker.paths = files.map(file => file.path).join("\n");
 		}
 	}
-	function getChosenFiles() {
-		//todo: pull in the file info - such as duration, artist, song/video name
-		var files = [];
-
-		if ($scope.filePicker.urlPath && $scope.filePicker.urlPath.length > 0) {
-			try {
-				new URL($scope.filePicker.urlPath);
-			}
-			catch (e) {
-				//todo: invalid url should report that to user and abort
-			}
-			$scope.filePicker.url = {
-				path: $scope.filePicker.urlPath, 
-				name: $scope.filePicker.urlPath.replace(/^.*(\\|\/|\:)/, '')//set file name from url
-			};
-			
-			files.push($scope.filePicker.url);
-		}
-		else if ($scope.filePicker.localFiles && $scope.filePicker.localFiles.length > 0)
-			files = $scope.filePicker.localFiles;
-		else return []; //no files are selected and url isn't set so don't do anything, just return
-
-		$scope.filePicker = {}; //clear out the url and local file fields
-
-		return files;
-	}
-	$scope.launch = function launch(file) {
-		//if a file is passed in, use that. Otherwise pull from the file picker box
-		var files = [];
-
-		if (!file) // no file is passed in, try to get a file from the file picker box
-		{
-			files = $scope.addToPlaylist();
-			if (files.length == 0) return null; //no file in the picker box either, return null.  todo: probably handle this better
-		}
-		else
-			files.push(file);
-
-		//todo: this is going to cause the same files to be mapped more than once
-		//should make sure on the backend that if the file is already mapped we don't map it again
-		//also should throw in something random into the path of the map in case someone wants to
-		//play two files of the same name --maybe throw a folder with the name of a guid in the path
-		eventService.emit('launch', $scope.activeDevice, files[0]);
-		$scope.activeFile = files[0];
-	};
-	$scope.addToPlaylist = function addToPlaylist() {
-		//Adds files to playlist from file picker box and returns the added files
-		var files = getChosenFiles();
-		files.forEach(file => $scope.playlist.push(file));
-
-		return files;
-	};
-	$scope.removeFromPlaylist = function removeFromPlaylist(file) {
-		for (var i in $scope.playlist)
-			if ($scope.playlist.hasOwnProperty(i) && $scope.playlist[i] === file) {
-				if (file === $scope.activeFile) {
-					if ($scope.playlist.length > 1) $scope.next();
-					else $scope.stop();
-				}
-				$scope.playlist.splice(i, 1);
-				break;
-			}
-	};
-	$scope.clearPlaylist = function clearPlaylist() {
-		$scope.playlist = [];
-		$scope.stop();
-	};
-	$scope.randomizePlaylist = function randomizePlaylist() {
-		shuffle($scope.playlist);
-	};
-	$scope.setName = function setName(name) {
-		eventService.emit('setName', $scope.activeDevice, name);
-	};
-
 	function getNameFromUrl(url) {
 		if (!(url instanceof URL)) url = new URL(url);
 		return url.pathname.replace(/^.*(\\|\/|\:)/, '');
 	}
+	function shuffle(array) {
+		///Fisher–Yates Shuffle
+		var currentIndex = array.length, temporaryValue, randomIndex;
+
+		// While there remain elements to shuffle...
+		while (0 !== currentIndex) {
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			// And swap it with the current element.
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+
+		return array;
+	}
+
+
+	$scope.setName = function setName(name) {
+		eventService.emit('setName', $scope.activeDevice, name);
+	};
+
+
+	$scope.refreshDevices = function refreshDevices() {
+		eventService.emit('refreshDevices');
+	};
+	$scope.loadDevices = function loadDevices() {
+		eventService.emit("loadDevices");
+	};
 	function addUpdateDevice(device) {
 		var found = false;
 		$scope.devices.forEach(function (scopeDevice) {
@@ -205,18 +294,21 @@ rotaryApp.controller('DeviceController', function DeviceController($scope, $rout
 		if (!found) {
 			device.services.forEach(service => service.htmlId = service.id.replace(/[\:\.]/g, ""));
 			$scope.devices.push(device);
-		}
 
-		if (device.id === $routeParams.deviceId) {
-			$scope.activeDevice = device;
-			if (device.videoCapable)
-				$scope.protocolInfoFilter = 'video';
-			else if (device.audioCapable)
-				$scope.protocolInfoFilter = 'audio';
-			else if (device.imageCapable)
-				$scope.protocolInfoFilter = 'image';
-			else if (device.mirrorCapable)
-				$scope.protocolInfoFilter = 'mirror';
+			if (device.id === $routeParams.deviceId) {
+				$scope.activeDevice = device;
+
+				if (!$scope.protocolInfoFilter) { //todo: I don't like this.  Find a better way to show the first available capability
+					if (device.videoCapable)
+						$scope.protocolInfoFilter = 'video';
+					else if (device.audioCapable)
+						$scope.protocolInfoFilter = 'audio';
+					else if (device.imageCapable)
+						$scope.protocolInfoFilter = 'image';
+					else if (device.mirrorCapable)
+						$scope.protocolInfoFilter = 'mirror';
+				}
+			}
 		}
 
 		addTypes(device);
@@ -240,49 +332,6 @@ rotaryApp.controller('DeviceController', function DeviceController($scope, $rout
 		$scope.deviceTypes = $scope.deviceTypes.filter(deviceType => $scope.devices.some(device => device.type.name === deviceType.name && device.type.urn === deviceType.urn));
 		$scope.serviceTypes = $scope.serviceTypes.filter(serviceType => $scope.devices.some(device => device.services.some(service => service.type.name === serviceType.name && service.type.urn === serviceType.urn)));
 	}
-	function setActiveFile(fileUri) {
-		if (fileUri != null) {
-			var file = getFile(fileUri);
-
-			if (file == null) {
-				file = { name: decodeURI(getNameFromUrl(fileUri)), path: fileUri };
-				//$scope.playlist.push(file);
-			}
-		}
-		//$scope.activeFile = file;
-
-		return $scope.activeFile;
-	}
-	function getFile(fileUri) {
-		var file = $scope.playlist == null ? null : $scope.playlist.filter(item => item.path === fileUri);
-		return file == null ? null : file[0];
-	}
-	function setFileInformation(event) {
-		var fileUri;
-
-		if (event.hasOwnProperty('CurrentTrackURI'))
-			fileUri = event.CurrentTrackURI;
-		else if (event.hasOwnProperty('AVTransportURI'))
-			fileUri = event.AVTransportURI;
-
-		var file = getFile(fileUri);
-		setActiveFile(fileUri);
-
-		if (file == null) return;
-
-		if (event.hasOwnProperty('CurrentTrackMetaData'))
-			file.metadata = event.CurrentTrackMetaData;
-		else if (event.hasOwnProperty('AVTransportURIMetaData'))
-			file.metadata = event.AVTransportURIMetaData;
-
-		if (event.hasOwnProperty('CurrentTrackDuration'))
-			file.duration = event.CurrentTrackDuration;
-		else if (event.hasOwnProperty('CurrentMediaDuration'))
-			file.duration = event.CurrentMediaDuration;
-
-		if (event.hasOwnProperty('CurrentTrack'))
-			file.trackNumber = event.CurrentTrack;
-	}
 	function setDeviceProperties(event) {
 		if (event.hasOwnProperty('Mute'))
 			$scope.activeDevice.isMuted = event.Mute === "1";
@@ -302,6 +351,8 @@ rotaryApp.controller('DeviceController', function DeviceController($scope, $rout
 		if (event.hasOwnProperty('CurrentTransportActions'))
 			$scope.availableActions = event.CurrentTransportActions.split(",");
 	}
+
+
 	function fractionToFloat(fraction) {
 		var y = fraction.split(' ');
 		if (y.length > 1) {
@@ -315,9 +366,12 @@ rotaryApp.controller('DeviceController', function DeviceController($scope, $rout
 		}
 	}
 	function eventOccured(device, events, request) {
-		if (!events) { console.log("event is undefined"); return; }
-		if (device.id != $scope.activeDevice.id) { console.log("event is not for current device.  Ignoring."); return; }
+		if (!events) { /*console.log("event is undefined");*/ return; }
+		if (device.id != $scope.activeDevice.id) { /*console.log("event is not for active device. Ignoring.");*/ return; }
+
 		events.forEach(event => {
+
+			$window.toastr["info"](JSON.stringify(event));
 
 			setDeviceProperties(event);
 			setFileInformation(event);
@@ -387,9 +441,10 @@ rotaryApp.controller('DeviceController', function DeviceController($scope, $rout
 			if (event.hasOwnProperty('TransportPlaySpeed'))
 				$scope.playback.speed = fractionToFloat(event.TransportPlaySpeed);
 
-			console.log(JSON.stringify(event));
 		});
 	}
+
+
 
 	eventService.on('deviceLost', removeDevice);
 	eventService.on('deviceFound', addUpdateDevice);
@@ -398,24 +453,45 @@ rotaryApp.controller('DeviceController', function DeviceController($scope, $rout
 
 	eventService.emit("loadDevices");
 
-	function shuffle(array) {
-		///Fisher–Yates Shuffle
-		var currentIndex = array.length, temporaryValue, randomIndex;
+	
+	$scope.clearToastr = function clearToastr() {
+		$window.toastr.clear();
+	};
+	$window.toastr.options = {
+		"newestOnTop": false,
+		"positionClass": "toast-bottom-right",
+		"preventDuplicates": false,
+		"onclick": null,
+		"showDuration": "300",
+		"hideDuration": "1000",
+		"timeOut": 0,
+		"extendedTimeOut": 0,
+		"showEasing": "swing",
+		"hideEasing": "linear",
+		"showMethod": "fadeIn",
+		"hideMethod": "fadeOut",
+		"tapToDismiss": true
+	};
+	
 
-		// While there remain elements to shuffle...
-		while (0 !== currentIndex) {
-			// Pick a remaining element...
-			randomIndex = Math.floor(Math.random() * currentIndex);
-			currentIndex -= 1;
+	$scope.$on('keydown', function (g, event) {
+		if (event.target.tagName.toLowerCase() === "input") return;
 
-			// And swap it with the current element.
-			temporaryValue = array[currentIndex];
-			array[currentIndex] = array[randomIndex];
-			array[randomIndex] = temporaryValue;
+		switch (event.key.toLowerCase()) {
+			case " ":
+				$scope.togglePlayState($scope.activeDevice);
+				break;
+			case "arrowright":
+				$scope.next();
+				break;
+			case "arrowleft":
+				$scope.previous();
+				break;
+			case "delete":
+				if ($scope.currentTrack.activeFile) $scope.removeFromPlaylist($scope.currentTrack.activeFile);
+				break;
 		}
-
-		return array;
-	}
+	});
 
 	//window.loadTestDevices();
 });
