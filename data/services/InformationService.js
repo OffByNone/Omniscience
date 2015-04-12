a1bb2e0a822fcf7fb1fb00665e2e9f33c43dde5c
OffByNone <jeremy.mcneil@gmail.com>
2015-04-11 22:31:51 -0500

    Changed information service to a factory

--- data/services/InformationService.js
+++ data/services/InformationService.js
@@ -1,4 +1,4 @@
-﻿omniscience.service('informationService', function informationService() {
+﻿omniscience.factory('informationService', function informationService() {
 	"use strict";
 
 	var _services = {}; //key is service.type.urn
