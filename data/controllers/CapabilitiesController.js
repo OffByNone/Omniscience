72243b6a20dc2d9fc37ece6f36b237461e59c80b
OffByNone <jeremy.mcneil@gmail.com>
2015-04-25 23:12:45 -0500

    Cleaned up the logo, Service name, Id and device name
    Also cleaned up the capabilities displaying of objects

--- data/controllers/CapabilitiesController.js
+++ data/controllers/CapabilitiesController.js
@@ -16,6 +16,18 @@
 	connectionManagerService.getProtocolInfo().then(protocolInfo => {
 		$scope.mediums = protocolInfo.map(x => x.contentFormat.medium).filter((value, index, self) => self.indexOf(value) === index); //todo: better/faster/easier way to get unique medium values
 		$scope.protocolInfo = protocolInfo;
+		$scope.protocolInfo.forEach(item => {
+			item.additionalInfoFlags = [];
+			if (item.additionalInfo && typeof item.additionalInfo.flags === "object") {
+				for (var key in item.additionalInfo.flags) {
+					if (item.additionalInfo.flags[key]) {
+						item.additionalInfoFlags.push(key);
+					}
+				}
+				delete item.additionalInfo.flags;
+			}
+		});
+
 		$scope.protocolInfoFilter = $scope.mediums[0];
 	});
 
