c3e1ba2e21bd1619a556b423cfbec707c5c42d9a
OffByNone <jeremy.mcneil@gmail.com>
2015-05-04 17:26:48 -0500

    .apply is no more!
    replaced with rest parameters and spread operators
    rebuilt xpi

--- data/services/EventService.js
+++ data/services/EventService.js
@@ -1,69 +1,61 @@
-omniscience.factory('eventService', function($rootScope, $window, $q){
-	var serviceResponsePromises = {};
-	var emitPromises = {};
+omniscience.factory('eventService', function ($rootScope, $window, $q) {
+    var serviceResponsePromises = {};
+    var emitPromises = {};
 
-	function generateQuickGuidish() { //e7 from http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-		var lut = []; for (var i = 0; i < 256; i++) { lut[i] = (i < 16 ? '0' : '') + (i).toString(16); }
-		var d0 = Math.random() * 0xffffffff | 0;
-		var d1 = Math.random() * 0xffffffff | 0;
-		var d2 = Math.random() * 0xffffffff | 0;
-		var d3 = Math.random() * 0xffffffff | 0;
-		return lut[d0 & 0xff] + lut[d0 >> 8 & 0xff] + lut[d0 >> 16 & 0xff] + lut[d0 >> 24 & 0xff] + '-' +
+    function generateQuickGuidish() { //e7 from http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+        var lut = []; for (var i = 0; i < 256; i++) { lut[i] = (i < 16 ? '0' : '') + (i).toString(16); }
+        var d0 = Math.random() * 0xffffffff | 0;
+        var d1 = Math.random() * 0xffffffff | 0;
+        var d2 = Math.random() * 0xffffffff | 0;
+        var d3 = Math.random() * 0xffffffff | 0;
+        return lut[d0 & 0xff] + lut[d0 >> 8 & 0xff] + lut[d0 >> 16 & 0xff] + lut[d0 >> 24 & 0xff] + '-' +
 			lut[d1 & 0xff] + lut[d1 >> 8 & 0xff] + '-' + lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & 0xff] + '-' +
 			lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + '-' + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] +
 			lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff];
-	}
-	function on(messageName, callback){
-		$window.self.port.on(messageName, function(){
-			$rootScope.$apply(callback.apply(this, arguments));
-		});
-	}
-	function emit(messageName) {
-		//accepts n number of arguments, messageName must be first
-		var uniqueId = generateQuickGuidish();
-		var deferred = $q.defer();
+    }
+    function on(messageName, callback) {
+        $window.self.port.on(messageName, (...args) => $rootScope.$apply(callback(...args)));
+    }
+    function emit(messageName, ...args) {
+        var uniqueId = generateQuickGuidish();
+        var deferred = $q.defer();
 
-		emitPromises[uniqueId] = deferred;
-		var args = Array.prototype.slice.call(arguments);
-		args = args.slice(1, args.length);
-		var args = [messageName, uniqueId].concat(args);
-		$window.self.port.emit.apply(this, args);
-		return deferred.promise;
-	}
+        emitPromises[uniqueId] = deferred;
+        $window.self.port.emit(messageName, uniqueId, ...args);
+        return deferred.promise;
+    }
 
-	on("emitResponse", function (uniqueId) {
-		//accepts n number of arguments, uniqueId must be first
-		var deferred = emitPromises[uniqueId];
-		if (deferred) {
-			delete emitPromises[uniqueId];
-			var args = Array.prototype.slice.call(arguments);
-			args = args.slice(1, args.length);
-			deferred.resolve.apply(deferred, args);
-		}
-		else console.log("no deferred for the response");
-	});
-	on("CallServiceResponse", function (uniqueId, data) {
-		var deferred = serviceResponsePromises[uniqueId];
-		if (deferred) {
-			delete serviceResponsePromises[uniqueId];
-			deferred.resolve(data);
-		}
-		else console.log("no deferred for the response");
-	});
+    on("emitResponse", function (uniqueId, ...args) {
+        //accepts n number of arguments, uniqueId must be first
+        var deferred = emitPromises[uniqueId];
+        if (deferred) {
+            delete emitPromises[uniqueId];
+            deferred.resolve(...args);
+        }
+        else console.log("no deferred for the response");
+    });
+    on("CallServiceResponse", function (uniqueId, data) {
+        var deferred = serviceResponsePromises[uniqueId];
+        if (deferred) {
+            delete serviceResponsePromises[uniqueId];
+            deferred.resolve(data);
+        }
+        else console.log("no deferred for the response");
+    });
 
-	function callService(service, serviceMethod, data) {
-		var uniqueId = generateQuickGuidish();
-		var deferred = $q.defer();
+    function callService(service, serviceMethod, data) {
+        var uniqueId = generateQuickGuidish();
+        var deferred = $q.defer();
 
-		serviceResponsePromises[uniqueId] = deferred;
+        serviceResponsePromises[uniqueId] = deferred;
 
-		$window.self.port.emit("CallService", uniqueId, service, serviceMethod, data);
-		return deferred.promise;
-	}
+        $window.self.port.emit("CallService", uniqueId, service, serviceMethod, data);
+        return deferred.promise;
+    }
 
-	return {
-		on: on,
-		emit: emit,
-		callService: callService
-	};
+    return {
+        on: on,
+        emit: emit,
+        callService: callService
+    };
 });
--- data/services/PubSub.js
+++ data/services/PubSub.js
@@ -1,15 +1,10 @@
 ﻿omniscience.factory('pubSub', function ($rootScope) {
 	return {
-		pub: function (message, data) {
-			//will take in n number of data params
-			$rootScope.$emit.apply($rootScope, arguments); //todo: no idea if this is correct. apply(this, arguments) didn't work so I changed to apply($rootScope,arguments) and it seems to work...
+		pub: function (...args) {
+			$rootScope.$emit(...args);
 		},
 		sub: function (message, func, scope) {
-			var unbind = $rootScope.$on(message, function () {
-				var args = Array.prototype.slice.call(arguments);
-				args = args.slice(1, args.length);
-				func.apply(this, args);
-			});
+			var unbind = $rootScope.$on(message, (unused, ...args) => func(...args));
 			if (scope) scope.$on('$destroy', unbind);
 		}
 	};
