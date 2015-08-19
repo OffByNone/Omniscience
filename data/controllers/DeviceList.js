fe9c85b952f542351957a3fe7e8ef8a4b65b8426
OffByNone <jeremy.mcneil@gmail.com>
2015-08-18 19:31:05 -0500

    moved the search from the button press to the page load.  updated xpi

--- data/controllers/DeviceList.js
+++ data/controllers/DeviceList.js
@@ -1,4 +1,5 @@
 ﻿omniscience.controller('DeviceListController', function DeviceListController($scope, eventService, $q) {
 	"use strict";
 	eventService.emit("loadDevices");
+	eventService.emit("refreshDevices");
 });
